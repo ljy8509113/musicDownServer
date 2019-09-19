31e82da8e8eb21e4fcb49b93d9813956c89e1794
ljy <ljy8509113@nate.com>
2019-09-19 17:33:00 +0900

    ing

--- MusicEJS/routes/index.js
+++ MusicEJS/routes/index.js
@@ -3,10 +3,20 @@ var router = express.Router();
 var cheerio = require('cheerio');
 var request = require('request');
 
- var url = 'https://www.melon.com/chart/';
-var title = new Array();
-var artist = new Array();
+var Youtube = require('youtube-node');
+var youtube = new Youtube();
+
+var url = 'https://www.melon.com/chart/';
 var rank = 100;
+var arrayRank = new Array();
+class MusicRank {
+  constructor(rank, title, artist) {
+        this.rank = rank;
+        this.title = title;
+        this.artist = artist;
+    }
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('index', { title: 'test' });
@@ -14,7 +24,8 @@ router.get('/', function(req, res, next) {
 
 request(url, function(error, response, html){
 	var $ = cheerio.load(html);
-	
+	var title = new Array();
+  var artist = new Array();
 // 곡명 파싱
     for (var i = 0; i < rank; i++) {
       $('.ellipsis.rank01 > span > a').each(function(){
@@ -37,9 +48,20 @@ request(url, function(error, response, html){
 
 	for (var i = 0; i < rank; i++) {
       console.log((i+1)+ "위" + " " + title[i] + " - " + artist[i]);
+      var info = new MusicRank(i+1, title[i], artist[i]);
+      console.log("info : " + info.title);
+      arrayRank[i] = info;
+      i++;
     }
+
+    search(arrayRank)
+
+    console.log("arrayRank : " + arrayRank.length);
     
 });
 
+function search(array){
+
+}
 
 module.exports = router;
